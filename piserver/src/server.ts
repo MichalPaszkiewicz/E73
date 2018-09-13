52be53b407ba90a92bacde45085ff79748aeaa8e
tametheboardgame <fairbairn.dave@yahoo.co.uk>
2018-09-13 21:11:53 +0100

    adding logging

--- piserver/src/server.ts
+++ piserver/src/server.ts
@@ -13,6 +13,7 @@ import { DefaultControlModule } from "./framework/services/defaultcontrolmodule"
 import { MotorEventHandler } from "./hats/motozero/motoreventhandler";
 import { TwoWheelDriveCommandHandler } from "./commandhandlers/twowheeldrivecommandhandler";
 import { LearningService } from "./services/learningservice";
+import { LogEventHandler } from "./eventhandlers/logeventhandler";
 
 var motor1 = motorFactory("motor1", 5, 24, 27);
 var motor2 = motorFactory("motor2", 17, 6, 22);
@@ -24,6 +25,10 @@ var motorEventHandler = new MotorEventHandler([motor1, motor2]);
 var domainService = new DefaultDomainService();
 var controlModule = new DefaultControlModule(domainService);
 controlModule.registerRobotEventHandler(motorEventHandler);
+
+var logEventHandler = new LogEventHandler();
+controlModule.registerRobotEventHandler(logEventHandler);
+
 controlModule.registerCommandHandler(new TwoWheelDriveCommandHandler());
 
 var learningService = new LearningService();
