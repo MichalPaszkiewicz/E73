73a77eaab1d42e9b6e67b504ee01bb1229e3fa19
Mehow <michalpaszkiewicz@hotmail.com>
2018-09-24 18:49:50 +0100

    setup default left and right motors in server.ts

--- piserver/src/server.ts
+++ piserver/src/server.ts
@@ -19,12 +19,12 @@ import { Environment } from "./helpers/environment";
 
 //need to define motors with "leftMotor" and "rightMotor" ids for TwoWheelDrive
 
-var motor1 = motorFactory("motor1", 5, 24, 27);
+var motor1 = motorFactory("rightMotor", 5, 24, 27);
 var motor2 = motorFactory("motor2", 17, 6, 22);
 var motor3 = motorFactory("motor3", 12, 23, 16);
-var motor4 = motorFactory("motor4", 25, 13, 18);
+var motor4 = motorFactory("leftMotor", 25, 13, 18);
 
-var motorEventHandler = new MotorEventHandler([motor1, motor2]);
+var motorEventHandler = new MotorEventHandler([motor1, motor4]);
 
 var domainService = new DefaultDomainService();
 var controlModule = new DefaultControlModule(domainService);
